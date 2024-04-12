9d4c8e79ddb206eefaacbd0b2617f5ad173056fe
Tomáš Martinčík <tomas.martincik@seznam.cz>
2024-04-13 00:18:11 +0200

    Fix hardcoded api url

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,10 @@
+import os
+
 import pytest_asyncio
 from httpx import AsyncClient
 
 
 @pytest_asyncio.fixture
 async def async_client():
-    async with AsyncClient(base_url="http://app:8000") as client:
+    async with AsyncClient(base_url=os.environ.get("API_URL")) as client:
         yield client
